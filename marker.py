7d6f4ac9cc7546a5c72cd2aca0bd6ccb608aff79
lochungtin <lochungtin@gmail.com>
2025-08-07 04:02:38 +0100

    1000x speed upgrade

--- marker.py
+++ marker.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-import sympy as sp
 from PIL import Image
 
 
@@ -12,7 +11,8 @@ class Marker:
         self.midpoint = self.vertices.mean(axis=0).astype(int)
         self.normalised = (self.vertices - self.midpoint).astype(int)
         self.edges = [
-            np.linalg.norm(self.vertices[a] - self.vertices[b]) for a, b in Marker.EDGE_MAP
+            np.linalg.norm(self.vertices[a] - self.vertices[b])
+            for a, b in Marker.EDGE_MAP
         ]
 
         self.edgeL = None
@@ -66,18 +66,18 @@ class Marker:
         if self.edgeL == None:
 
             def step(a, b):
-                z0, z1 = sp.symbols("z:2")
-                eq0 = sp.Eq(z0**2 - z1**2, b[0] ** 2 + b[1] ** 2 - a[0] ** 2 - a[1] ** 2)
-                eq1 = sp.Eq(z0 * z1, -a[0] * b[0] - a[1] * b[1])
-                res = sp.solve([eq0, eq1], (z0, z1))[0]
-                z0, z1 = res[0].evalf(), res[1].evalf()
-
-                p0 = np.float32([a[0], a[1], z0])
-                p1 = np.float32([b[0], b[1], z0])
+                A = b[0] ** 2 + b[1] ** 2 - a[0] ** 2 - a[1] ** 2
+                B = -a[0] * b[0] - a[1] * b[1]
+                x = np.sqrt((A + np.sqrt(A**2 + (4 * B**2))) / 2)
+                p0 = np.float32([a[0], a[1], x])
+                p1 = np.float32([b[0], b[1], x])
                 return np.linalg.norm(p0 - p1)
 
             self.edgeL = np.mean(
-                [step(self.normalised[a], self.normalised[b]) for a, b in Marker.EDGE_MAP]
+                [
+                    step(self.normalised[a], self.normalised[b])
+                    for a, b in Marker.EDGE_MAP
+                ]
             )
 
         return self.edgeL
@@ -110,6 +110,6 @@ class Marker:
 
     def __str__(self):
         return str(self.vertices)
-    
+
     def __repr__(self):
         return self.__str__()
--- utils.py
+++ utils.py
@@ -27,8 +27,6 @@ ARUCO_DICT = {
     "DICT_APRILTAG_36h11": arc.DICT_APRILTAG_36h11,
 }
 
-CORNER2EDGE_MAP = [[0, 1], [1, 2], [2, 3], [3, 0]]
-
 
 def loadArUcoDict(mType):
     if mType in ARUCO_DICT:
@@ -50,20 +48,3 @@ def saveCSV(header, rows, fn):
     with open(fn, "w") as w:
         for r in [header] + rows:
             w.write(f"{', '.join(r)}\n")
-
-
-def dsc(i0, i1):
-    i0 = (i0 == 255).astype(int)
-    i1 = (i1 == 255).astype(int)
-    return np.sum(i1[i0 == 1]) * 2.0 / (np.sum(i0) + np.sum(i1))
-
-
-def angDist(R0, R1):
-    relR = np.dot(R0.T, R1)
-    traceR = np.trace(relR)
-    ang = np.clip((traceR - 1) / 2, -1.0, 1.0)
-    return np.degrees(np.arccos(ang))
-
-
-def toHex(n):
-    return str(hex(n))[-1]
